47e4935cb22d8c667b3fc4287922814b99dfaf01
Husainwow <husain.f@wowinfobiz.com>
2021-11-04 14:48:04 +0530

    new test change

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var _ = require('lodash');
 
 var array = [1,2,3,4,5,6,7,8];
-console.log('answer:', _.without(array, 3))
+// console.log('answer:', _.without(array, 3))
 
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
@@ -22,3 +22,12 @@ function setGradient() {
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+var x = 10
+var y = 30
+
+if(x => 12 && y <= 31 ){
+	console.log("eligible")
+} else (
+	console.log('ineligible')
+)
